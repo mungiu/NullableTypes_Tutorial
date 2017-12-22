fcf80ea126ba5bd1cc6338a40a892b545f567651
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-12-22 06:36:40 +0100

    tutorial finalized

--- NullableTypes_Tutorial/Program.cs
+++ NullableTypes_Tutorial/Program.cs
@@ -10,6 +10,40 @@ namespace NullableTypes_Tutorial
     {
         static void Main(string[] args)
         {
+            //allows null to be assigned
+            Nullable<DateTime> date = null;
+            //same as above but shorter
+            DateTime? date1 = null;
+
+            Console.WriteLine($".GetValueOrDefault() " +    //compiles
+                $"{date.GetValueOrDefault()}");
+            Console.WriteLine($".HasValue {date.HasValue}");//compiles
+            //Console.WriteLine($".Value {date.Value}\n");  //compile error use ".GetValueOrDefault"
+
+            DateTime? newDate = new DateTime(2017, 12, 25);
+            //DateTime newDate2 = newDate;    //doesn't work (what is newDate is null)
+            DateTime newDate2 = newDate.GetValueOrDefault();
+            DateTime? newDate3 = newDate2;  //NOTE: compatibility
+
+            //////////////////// NULL COLESCING OPERATOR "??" /////////////////////
+            DateTime? date3 = null;
+            DateTime date4;
+
+            //if (date3 != null)
+            //    date4 = date3.GetValueOrDefault();
+            //else
+            //    date3 = DateTime.Today;
+
+            //// ? - THE CONDITIONAL OPERATOR
+            //date4 = (date3 != null) ? date.GetValueOrDefault()
+                //: DateTime.Today;
+
+            //?? - the NULL COLESCING OPERATOR. 
+            // returns the left-hand operand if the operand is not null; 
+            // otherwise it returns the right hand operand.
+            date4 = date3 ?? DateTime.Today;
+
+            Console.WriteLine(date4);
         }
     }
 }
